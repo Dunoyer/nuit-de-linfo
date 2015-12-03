4d410f59533211fe165b706b239f87c9afc19ed6
Dunoyer <dunoyer.yoan@gmail.com>
2015-12-03 23:00:44 +0100

    Création test getJson API Cdiscount

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,6 +7,8 @@ import views.html.*;
 
 public class Application extends Controller {
 
+    private static String APIKey = "93cf730f-a372-4b74-8df3-e64bf9c7a817";
+
     public Result index() {
         return ok(index.render("Your new application is ready. Saadi le magnifique ! test de modification Valentino Team-MIA"));
     }
--- app/controllers/Client.java
+++ app/controllers/Client.java
@@ -28,8 +28,73 @@ public class Client {
             "  \"CartRequest\": {}\n" +
             "}";
 
-    public static void main(String args[]) {
+    public static String keyWORD ="tablette";
+
+    public static String JSONToSendToSearchMethod = "{\r\n  \"ApiKey\": \"93cf730f-a372-4b74-8df3-e64bf9c7a817\",\r\n  \"SearchRequest\": {\r\n    \"Keyword\": \"" + keyWORD + "\",\r\n    \"SortBy\": \"minprice\",\r\n    \"Pagination\": {\r\n      \"ItemsPerPage\": 1\r\n    },\r\n    \"Filters\": {\r\n      \"Price\": {\r\n        \"Min\": 0\r\n      },\r\n      \"IncludeMarketPlace\": false\r\n    }\r\n  }\r\n}";
+
+    public static String URLToSendToSearchMethod ="https://api.cdiscount.com/OpenApi/json/Search";
+
+    public static void toto(){
         OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/octet-stream");
+        RequestBody body = RequestBody.create(mediaType, "{\r\n  \"ApiKey\": \"93cf730f-a372-4b74-8df3-e64bf9c7a817\",\r\n  \"SearchRequest\": {\r\n    \"Keyword\": \"" + keyWORD + "\" ,\r\n    \"SortBy\": \"minprice\",\r\n    \"Pagination\": {\r\n      \"ItemsPerPage\": 1\r\n    },\r\n    \"Filters\": {\r\n      \"Price\": {\r\n        \"Min\": 0\r\n      },\r\n      \"IncludeMarketPlace\": false\r\n    }\r\n  }\r\n}");
+        Request request = new Request.Builder()
+                .url("https://api.cdiscount.com/OpenApi/json/Search")
+                .post(body)
+                .addHeader("cache-control", "no-cache")
+                .addHeader("postman-token", "c296c810-8c68-fbba-2c8c-b169fd5f09a0")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String createJsonForSearch(String keyWord, String sortBy){
+        return "{\r\n  \"ApiKey\": \"93cf730f-a372-4b74-8df3-e64bf9c7a817\",\r\n  \"SearchRequest\": {\r\n    \"Keyword\": \"" + keyWord + "\",\r\n    \"SortBy\": \"minprice\",\r\n    \"Pagination\": {\r\n      \"ItemsPerPage\": 1\r\n    },\r\n    \"Filters\": {\r\n      \"Price\": {\r\n        \"Min\": 0\r\n      },\r\n      \"IncludeMarketPlace\": false\r\n    }\r\n  }\r\n}";
+    }
+
+    public String JSON;
+    public static String searchProduct(String keyword, String sortBy)
+    {
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, createJsonForSearch(keyword, ""));
+        Request request = new Request.Builder()
+                .url(URLToSendToSearchMethod)
+                .post(body)
+                .addHeader("content-type", "application/json")
+                .build();
+
+        try {
+
+            Response response = client.newCall(request).execute();
+            InputStream stream = response.body().byteStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            StringBuilder result = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+
+            System.out.println(result.toString());
+            return result.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "erreur";
+        }
+    }
+
+    public static void main(String args[]) {
+
+        searchProduct("tablette","");
+
+        /*OkHttpClient client = new OkHttpClient();
 
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(mediaType, jsonToSend);
@@ -54,7 +119,7 @@ public class Client {
             System.out.println(result.toString());
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
 

cc1e631a2d695a62493b25b4cce8239873044523
Dunoyer <dunoyer.yoan@gmail.com>
2015-12-03 21:32:04 +0100

    Création test getJson API Cdiscount

--- app/client/Client.java
+++ app/client/Client.java
@@ -27,46 +27,14 @@ public class Client {
             "  \"ApiKey\": \"93cf730f-a372-4b74-8df3-e64bf9c7a817\",\n" +
             "  \"CartRequest\": {}\n" +
             "}";
-    public static HashMap<String,String> map = new HashMap<String,String>();
-
-    public static String nom;
-    public static String lieu;
-    public static String date;
-
-    private  String postToURL(DefaultHttpClient httpClient) throws IOException, IllegalStateException, UnsupportedEncodingException, RuntimeException {
-        HttpPost postRequest = new HttpPost(UPDATE_URL);
-
-        StringEntity input = new StringEntity(jsonToSend);
-        input.setContentType("application/json");
-        postRequest.setEntity(input);
-
-        HttpResponse response = httpClient.execute(postRequest);
-
-        if (response.getStatusLine().getStatusCode() != 200) {
-            throw new RuntimeException("Failed : HTTP error code : "
-                    + response.getStatusLine().getStatusCode());
-        }
-
-        BufferedReader br = new BufferedReader(
-                new InputStreamReader((response.getEntity().getContent())));
-
-        String output;
-        StringBuffer totalOutput = new StringBuffer();
-        System.out.println("Output from Server .... \n");
-        while ((output = br.readLine()) != null) {
-            System.out.println(output);
-            totalOutput.append(output);
-        }
-        return totalOutput.toString();
-    }
 
     public static void main(String args[]) {
         OkHttpClient client = new OkHttpClient();
 
         MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\r\n  \"ApiKey\": \"93cf730f-a372-4b74-8df3-e64bf9c7a817\",\r\n  \"SearchRequest\": {\r\n    \"Keyword\": \"Television\",\r\n    \"SortBy\": \"rating\",\r\n    \"Pagination\": {\r\n      \"ItemsPerPage\": 5\r\n    },\r\n    \"Filters\": {\r\n      \"Price\": {\r\n        \"Min\": 0,\r\n        \"Max\": 500\r\n      },\r\n      \"IncludeMarketPlace\": true,\r\n      \"Brands\": [\r\n        \"Samsung\"\r\n      ],\r\n      \"Condition\": \"new\"\r\n    }\r\n  }\r\n}");
+        RequestBody body = RequestBody.create(mediaType, jsonToSend);
         Request request = new Request.Builder()
-                .url("https://api.cdiscount.com/OpenApi/json/Search")
+                .url(UPDATE_URL)
                 .post(body)
                 .addHeader("content-type", "application/json")
                 .build();
@@ -89,27 +57,5 @@ public class Client {
         }
     }
 
-    private static String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
-        StringBuilder result = new StringBuilder();
-        boolean first = true;
-        for(Map.Entry<String, String> entry : params.entrySet()){
-            if (first)
-                first = false;
-            else
-                result.append("&");
-
-            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-            result.append("=");
-            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-        }
-
-        return result.toString();
-    }
-
-    public static void setParams(String nom, String lieu, String date) {
-        map.put("nom", nom);
-        map.put("lieu",lieu);
-        map.put("date", date);
-    }
 
 }

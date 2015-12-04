284f10083594095032840b100b2dcc66c6f795d5
Dunoyer <dunoyer.yoan@gmail.com>
2015-12-04 04:35:04 +0100

    V

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,6 +9,10 @@ public class Application extends Controller {
 
     private static String APIKey = "93cf730f-a372-4b74-8df3-e64bf9c7a817";
 
+    public  Result infos() {
+        return ok(infos.render());
+    }
+
     public  Result seisme() {
         return ok(seisme.render());
     }
